a7109640b63224c7347b4f6d4956ffc71908e82b
Edwinn7 <you@example.com>
2022-05-11 21:19:00 -0500

    first commit

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,11 @@
 // importar modulos
 import { ampliarInfo } from "./ampliarInfo.js";
 import { pintarTienda } from "./llenadoTienda.js";
-// import { anadir } from "./anadir.js";
+import { pintarCarrito } from "./pintarCarrito.js";
+//import { resumencar } from "./resumencar.js";
 // llamar modulo de pintar
 pintarTienda()
+//pintarCarrito()
 
 
 let producto={}
@@ -17,7 +19,19 @@ contenedorTienda.addEventListener("click",function(event){
         producto=ampliarInfo(event)
         modalInfo.show()
     }
-} )
+})
+// // llamar mudulo resumen
+
+// let contenedorVenta=document.getElementById("fila")
+// contenedorVenta.addEventListener("click",function(event){
+//     if(event.target.classList.contains("btncarrito")){
+//         let modalInfo = new bootstrap.Modal(document.getElementById('resumenCar'))
+//         producto=resumencar(event)
+//         modalInfo.show()
+//     }
+// })
+
+//-----------------------------------------------//
 
 //carrito arreglo de productos
 let carrito=[]
@@ -25,31 +39,110 @@ let carrito=[]
 let boton = document.getElementById("btnAdd")
 boton.addEventListener('click',function(event){
     console.log(carrito)
+
     let cantidad=document.getElementById("cantProd").value
     // agregar cant obj prod
     producto.cantidad=cantidad
 
+    // adjuntar producto
+    for(let i =0; i < carrito.length ; i++){
+        if(carrito[i].nombre.trim()=== producto.nombre.trim()){
+            carrito[i].cantidad++
+            return null;
+        }
+    }
     //agar prod al crrto
     carrito.push(producto)
 
-    // calcular suma de cant 
+    // calcular suma de cant
 
     let suma=0
-
     carrito.forEach(function(producto){
         // console.log(producto.cantidad)
         suma=suma+Number(producto.cantidad)
     });
-    // console.log(suma)
+    pintarCarrito(suma)
+    
+    let subtotal=0
+        carrito.forEach(function(producto){
+        //subtotal=subtotal*Number(producto.precio)
+        //subtotal=cantidad*Number(producto.precio)
+        subtotal=cantidad*Number(producto.precio)
+        console.log("EL SUBTOTAL "+subtotal)
+    });
 
-    let capsula = document.getElementById("capsula")
-    capsula.textContent=suma
-    capsula.classList.remove("invisible")
 })
-    let boton2 = document.getElementById("btnLimp")
-    boton2.addEventListener("click",function(event){
-        carrito=[]
-        let capsula= document.getElementById("capsula")
-        capsula.textContent = 0
-        capsula.classList.add("invisible")
+let btnconvert = document.getElementById("btnconvert")
+btnconvert.addEventListener("click",function(event){
+    let cantidad=document.getElementById("cantProd").value
+    // agregar cant obj prod
+    producto.cantidad=cantidad
+    cantidad*Number(producto.precio)
+    console.log("EL SUBTOTAL "+cantidad)
+})
+
+
+    let btncarrito = document.getElementById("btncarrito")
+    btncarrito.addEventListener("click",function(event){
+        //console.log("carrito")
+
+        let contenedor=document.getElementById("contenedorVenta")
+        let modalVenta = new bootstrap.Modal(document.getElementById('resumenCar'))
+        // borrar el contenido HTML de una sección
+        contenedor.innerHTML=""
+        // recorrer carrrito para pintar
+
+        carrito.forEach(function(producto){
+            // traversing
+
+            let fila=document.createElement("div")
+            fila.classList.add("row")
+
+            let col=document.createElement("div")
+            col.classList.add("col-12","col-mb-4")
+
+            let col2=document.createElement("div")
+            col2.classList.add("col-12","col-mb-8")
+
+            let foto=document.createElement("img")
+            foto.classList.add("img-fluid","col-md-5")
+            foto.src=producto.foto
+
+            let titulo=document.createElement("p")
+            titulo.classList.add("col-md-7")
+            titulo.textContent="Producto: "+producto.nombre
+
+            let precio=document.createElement("p")
+            precio.classList.add("col-md-7")
+            precio.textContent="Valor Unitario: "+producto.precio
+
+            let cantidad=document.createElement("p")
+            cantidad.classList.add("col-md-7")
+            cantidad.textContent="Cantidad agregada: "+producto.cantidad
+
+            // let subtotal=document.createElement("p")
+            // subtotal.classList.add()
+            // subtotal.textContent=producto.subtotal
+            // subtotal.textContent=producto.subtotal
+
+
+            col.appendChild(foto)
+            col2.appendChild(titulo)
+            col2.appendChild(precio)
+            col2.appendChild(cantidad)
+            // col2.appendChild(subtotal)
+            //col2.appendChild(boton)
+            fila.appendChild(col)
+            fila.appendChild(col2)
+            contenedor.appendChild(fila)
+        })
+        modalVenta.show()
+        let btnlimcar = document.getElementById("btnlimcar")
+        btnlimcar.addEventListener("click",function(event){
+            carrito=[]
+            let capsula= document.getElementById("capsula")
+            capsula.textContent = 0
+            capsula.classList.add("invisible")
+        })
+
     })
--- js/llenadoTienda.js
+++ js/llenadoTienda.js
@@ -1,23 +1,23 @@
 export function pintarTienda(){
     let productos=[
-        {nombre:"Procesador gamer AMD Ryzen 5 5600",precio:"3.000.000 COP",foto:"img/productos/ryzen.jpg",descripscion:"Clave en el rendimiento de tu computador de escritorio, ya no tienes que pensar en cómo distribuir el tiempo y acciones porque ahora las tareas en simultáneo son posibles."},
-        {nombre:"MSI MAG B550 TORPEDO",precio:"617.000 COP",foto:"img/productos/msi.jpg",descripscion:"La serie MAG nació a través de rigurosas pruebas de calidad y está diseñada para ser un símbolo de robustez y durabilidad. Centrada en proporcionar la mejor experiencia de usuario, la serie MAG tiene un proceso de instalación simple junto con una interfaz de usuario amigable que la convierte en la mejor opción para los jugadores de nivel básico."},
+        {nombre:"Procesador gamer AMD Ryzen 5 5600",precio:3000000,foto:"img/productos/ryzen.jpg",descripscion:"Clave en el rendimiento de tu computador de escritorio, ya no tienes que pensar en cómo distribuir el tiempo y acciones porque ahora las tareas en simultáneo son posibles."},
+        {nombre:"MSI MAG B550 TORPEDO",precio:617000,foto:"img/productos/msi.jpg",descripscion:"La serie MAG nació a través de rigurosas pruebas de calidad y está diseñada para ser un símbolo de robustez y durabilidad. Centrada en proporcionar la mejor experiencia de usuario, la serie MAG tiene un proceso de instalación simple junto con una interfaz de usuario amigable que la convierte en la mejor opción para los jugadores de nivel básico."},
     
-        {nombre:"RAM corsair RGB 16gb DDR5",precio:"339.000 COP",foto:'img/productos/RAM.jpg',descripscion:"La memoria con overclocking DDR4 Corsair Vengeance RGB Pro ilumina el PC con un efecto hipnótico gracias a la iluminación RGB dinámica multizona, además de ofrecer las mejores características de las DDR4 en cuanto a rendimiento."},
+        {nombre:"RAM corsair RGB 16gb DDR5",precio:339000,foto:'img/productos/RAM.jpg',descripscion:"La memoria con overclocking DDR4 Corsair Vengeance RGB Pro ilumina el PC con un efecto hipnótico gracias a la iluminación RGB dinámica multizona, además de ofrecer las mejores características de las DDR4 en cuanto a rendimiento."},
     
-        {nombre:"fuente de alimentación corsair RM 750",precio:"418.000 COP",foto:"img/productos/RM750.jpg",descripscion:"Las fuentes de alimentación completamente modulares y de nivel sonoro ultrarreducido CORSAIR RM Series con varios conectores EPS12V proporcionan una potencia eficiente a su PC gracias a la certificación 80 PLUS Gold"},
+        {nombre:"fuente de alimentación corsair RM 750",precio:418000,foto:"img/productos/RM750.jpg",descripscion:"Las fuentes de alimentación completamente modulares y de nivel sonoro ultrarreducido CORSAIR RM Series con varios conectores EPS12V proporcionan una potencia eficiente a su PC gracias a la certificación 80 PLUS Gold"},
     
-        {nombre:"SSD Samsung 1TB PCIe3.0",precio:"555.000 COP",foto:"img/productos/SSD.jpg",descripscion:"La unidad Samsung 980 hereda el legado de la innovadora tecnología SSD de Samsung. Dispone de una fantástica fiabilidad"},
+        {nombre:"SSD Samsung 1TB PCIe3.0",precio:555000,foto:"img/productos/SSD.jpg",descripscion:"La unidad Samsung 980 hereda el legado de la innovadora tecnología SSD de Samsung. Dispone de una fantástica fiabilidad"},
     
-        {nombre:"Refrigeración Líquida Cooler Master",precio:"286.000 COP",foto:"img/productos/liquida.jpg",descripscion:"contiene  dos ventiladores PWM  de 120 mm RGB con una alta presión estática de 2,36  mm H2O."},
+        {nombre:"Refrigeración Líquida Cooler Master",precio:286000,foto:"img/productos/liquida.jpg",descripscion:"contiene  dos ventiladores PWM  de 120 mm RGB con una alta presión estática de 2,36  mm H2O."},
     
-        {nombre:"Disco duro SEAGATE 2TB SATA 3",precio:"220.000 COP",foto:"img/productos/discoduro.jpg",descripscion:"Seagate incorpora más de 20 años de rendimiento y fiabilidad dignos de confianza a las unidades de disco duro Seagate® BarraCuda® de 3,5 pulgadas."},
+        {nombre:"Disco duro SEAGATE 2TB SATA 3",precio:220000,foto:"img/productos/discoduro.jpg",descripscion:"Seagate incorpora más de 20 años de rendimiento y fiabilidad dignos de confianza a las unidades de disco duro Seagate® BarraCuda® de 3,5 pulgadas."},
     
-        {nombre:"Cooler Master Hyper 212 RGB",precio:"154.347 COP",foto:"img/productos/Cooler.jpg",descripscion:" ofrece una mejor instalación y un gran rendimiento, y es una de las mejores soluciones de enfriamiento de aire. "},
+        {nombre:"Cooler Master Hyper 212 RGB",precio:154347 ,foto:"img/productos/Cooler.jpg",descripscion:" ofrece una mejor instalación y un gran rendimiento, y es una de las mejores soluciones de enfriamiento de aire. "},
     
-        {nombre:"MSI Optix G241VC 23.6",precio:"829.064 COP",foto:"img/productos/Monitor.jpg",descripscion:" utiliza el panel curvo Samsung de alta calidad con muchas características especiales para los jugadores, lo que les ayudará a disfrutar del maravilloso mundo del juego."},
+        {nombre:"MSI Optix G241VC 23.6",precio:829064 ,foto:"img/productos/Monitor.jpg",descripscion:" utiliza el panel curvo Samsung de alta calidad con muchas características especiales para los jugadores, lo que les ayudará a disfrutar del maravilloso mundo del juego."},
     
-        {nombre:"HyperX Cloud II Auriculares Gaming 7.1 Rojos",precio:"396.892 COP",foto:"img/productos/auriculares.jpg",descripscion:"El diseño es totalmente gamer, agresivo, atrevido y sobre todo, molón de narices. Disfruta de unos auriculares gaming con calidad de estudio fabricados con materiales de alta calidad."}
+        {nombre:"HyperX Cloud II Auriculares Gaming 7.1 Rojos",precio:396892 ,foto:"img/productos/auriculares.jpg",descripscion:"El diseño es totalmente gamer, agresivo, atrevido y sobre todo, molón de narices. Disfruta de unos auriculares gaming con calidad de estudio fabricados con materiales de alta calidad."}
     ]
     
     // referencia al componente padre
@@ -64,7 +64,7 @@ export function pintarTienda(){
         // precio
         let precio=document.createElement("h7")
         precio.classList.add("modal-body")
-        precio.textContent=producto.precio
+        precio.textContent=" COP "+producto.precio
         // crear boton
         let boton=document.createElement("button")
         boton.setAttribute("type","button")
@@ -76,8 +76,8 @@ export function pintarTienda(){
     
         tarjeta.appendChild(foto)
         tarjeta.appendChild(titulo)
-        tarjeta.appendChild(precio)
         tarjeta.appendChild(descripscion)
+        tarjeta.appendChild(precio)
         tarjeta.appendChild(boton)
         columna.appendChild(tarjeta)
         fila.appendChild(columna)
